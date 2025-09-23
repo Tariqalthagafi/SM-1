8279a47c0769823af685298f076220750b232c89
Tariqzah <tariq.a.althagafi@zahsoft.com>
2025-09-23 12:44:26 +0300

    monitoring fixes2

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 import { injectSpeedInsights } from '@vercel/speed-insights'
-injectSpeedInsights()
+
 injectSpeedInsights()
 
 const app = createApp(App)
