c63c38a5db2f49f97434b34e857301ba64c55e43
Tariqzah <tariq.a.althagafi@zahsoft.com>
2025-11-10 20:52:22 +0300

    app changes p9

--- src/types/sitenew.ts
+++ src/types/sitenew.ts
@@ -1,9 +1,9 @@
 // ================== 📦 أنواع حسب الصفحات ==================
 export * from './contexts/index'
-export * from './contexts/home'
+export * from './contexts/home1'
 export * from './contexts/offers1'
 export * from './contexts/products1'
 export * from './contexts/sections1'
-export * from './contexts/settings'
+export * from './contexts/settings1'
 export * from './contexts/social1'
 export * from './contexts/templates1'
