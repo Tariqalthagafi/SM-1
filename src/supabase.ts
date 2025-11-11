4d2fddb445ac326191f69ae5cb5eb295dedd768f
Tariqzah <tariq.a.althagafi@zahsoft.com>
2025-11-11 03:39:51 +0300

    app changes p10

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 // src/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gpaeargjmygosmnqaees.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwYWVhcmdqbXlnb3NtbnFhZWVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwOTMwMzYsImV4cCI6MjA3NzY2OTAzNn0.ZLsmzlb-O848dbHSXanIL3_K37b8E81_tjfM7Pe2Zh8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
